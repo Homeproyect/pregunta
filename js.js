23961a58e41aad604d7f92fa0c038e556c7bd75d
Homeproyect <113480886+Homeproyect@users.noreply.github.com>
2023-08-06 19:48:20 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+const yes = document.querySelector('#yes');
+
+yes.addEventListener('click',function()
+{
+    alert('gracias sabia que dirias que si jaja 😂😂😂');
+});
+
+
+const no = document.querySelector('#no');
+
+no.addEventListener('mouseover',function()
+{
+   const randomX = parseInt(Math.random()*100);
+   const randomY = parseInt(Math.random()*100);
+   no.style.setProperty('top',randomY+'%');
+   no.style.setProperty('left',randomX+'%');
+   no.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
